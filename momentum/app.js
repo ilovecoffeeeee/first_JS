ba9e1c3c755176e0044ab59b5fd9d220f12351cb
sun_loves_Q <conconxel@gmail.com>
2021-06-25 00:38:26 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,34 @@
+const days0week = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-const a = 5;
-const b = 2;
-const myName = "sun" ;
+// Get Item form array
+console.log(days0week[5]);
+console.log(days0week);
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName)
+// Add one more day to the array
+days0week.push("sun");
+
+console.log(days0week);
+
+const player = {
+    name: "nico",
+    points : 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+player.fat= false;
+console.log(player);
+player.lastname = "kim"
+console.log(player);
+
+function sayHello(){
+    console.log("Hello my name is ")
+};
+
+alert();
+
+sayHello("A");
+sayHello("B");
+sayHello("C");
