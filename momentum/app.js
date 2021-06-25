20c00a7eefe3d6de37f408935ab9538e1fa95067
sun_loves_Q <conconxel@gmail.com>
2021-06-25 22:19:41 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,34 +1,23 @@
-const days0week = ["mon", "tue", "wed", "thu", "fri", "sat"];
-
-// Get Item form array
-console.log(days0week[5]);
-console.log(days0week);
-
-// Add one more day to the array
-days0week.push("sun");
-
-console.log(days0week);
-
-const player = {
-    name: "nico",
-    points : 10,
-    fat: true,
+const calculator = {
+    plus: function(a, b) {
+        return (a + b);
+    },
+    times: function(a, b) {
+        return (a * b);
+    },
+    minus: function(a, b) {
+        return (a - b);
+    },
+    divide: function(a, b) {
+        return (a / b);
+    },
+    power: function(a, b) {
+        return (a ** b);
+    },
 };
 
-console.log(player);
-console.log(player.name);
-
-player.fat= false;
-console.log(player);
-player.lastname = "kim"
-console.log(player);
-
-function sayHello(){
-    console.log("Hello my name is ")
-};
-
-alert();
-
-sayHello("A");
-sayHello("B");
-sayHello("C");
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
