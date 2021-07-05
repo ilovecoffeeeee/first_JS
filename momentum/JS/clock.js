8ed7cdfad95263b6ed778652cb0c823419819a13
isolovecoffee <86226153+isolovecoffee@users.noreply.github.com>
2021-07-05 20:21:00 +0900

    test 2

--- momentum/JS/clock.js
+++ momentum/JS/clock.js
@@ -8,4 +8,13 @@ function getClock() {
 getClock()
 setInterval(getClock, 1000);
 
-ddd
+const clock = document.querySelector('h2#clock');
+
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock()
+setInterval(getClock, 1000);
+
