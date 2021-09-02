7012705f0ddbdd0ef9b371cad7d35ebeb52ed208
sun_loves_Q <conconxel@gmail.com>
2021-09-02 22:02:12 +0900

    colors

--- momentum/JS/background.js
+++ momentum/JS/background.js
@@ -1,16 +1,28 @@
-const images = [
-    "0.png",
-    "1.png",
-    "2.png",
-    "3.png",
-];
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenColors = colors[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+bgColors.src = `${chosenColor}`;
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.appendChild(bgColors);
 
 
