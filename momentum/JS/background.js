38d43720c1190642688e82e15188d7bedb09060a
sun_loves_Q <conconxel@gmail.com>
2021-09-05 20:53:10 +0900

    Update background.js

--- momentum/JS/background.js
+++ momentum/JS/background.js
@@ -1,28 +1,16 @@
-const colors = [
-    "#ef5777",
-    "#575fcf",
-    "#4bcffa",
-    "#34e7e4",
-    "#0be881",
-    "#f53b57",
-    "#3c40c6",
-    "#0fbcf9",
-    "#00d8d6",
-    "#05c46b",
-    "#ffc048",
-    "#ffdd59",
-    "#ff5e57",
-    "#d2dae2",
-    "#485460",
-    "#ffa801",
-    "#ffd32a",
-    "#ff3f34"
-  ];
+const images = [
+    "0.png",
+    "1.png",
+    "2.png",
+    "3.png",
+];
 
 const chosenColors = colors[Math.floor(Math.random() * colors.length)];
 
-bgColors.src = `${chosenColor}`;
+const bgImage = document.createElement("img");
 
-document.body.appendChild(bgColors);
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
 
 
