d701ae2daf96a1c5678cef7ace08117c7fd06fd9
sun_loves_Q <conconxel@gmail.com>
2021-09-07 00:57:41 +0900

    todo again

--- momentum/JS/greeting.js
+++ momentum/JS/greeting.js
@@ -2,12 +2,29 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+
+
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASS);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username)
+};
+
+function paintGreeting(username) {
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS);
+};
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
 }
-
-
-loginForm.addEventListener("submit", onLoginSubmit)
